5ade622a9a30a0f2f7662cdf7dae6697312da57e
andresfgc2002 <afgonzalez73@ucatolica.edu.co>
2025-03-14 19:53:53 -0500

    logictest1

---
+++ logictest1.py
@@ -0,0 +1,58 @@
+class Logica:
+
+
+    def AND(self, a, b):
+       
+        return a and b
+
+logica = Logica()
+resultado = logica.AND(True, False)
+print(resultado) 
+
+pass
+
+class Logica:
+    
+    def AND(self, a, b):
+      
+        return a and b
+
+    def OR(self, a, b):
+        de a OR b
+        
+        return a or b
+
+
+logica = Logica()
+resultado_and = logica.AND(True, False)
+resultado_or = logica.OR(True, False)
+print(resultado_and) 
+print(resultado_or)  
+
+pass
+
+class Logica:
+    
+
+    def AND(self, a, b):
+        
+        return a and b
+
+    def OR(self, a, b):
+        
+        return a or b
+
+    def NOT(self, a):
+      
+        return not a
+
+
+logica = Logica()
+resultado_and = logica.AND(True, False)
+resultado_or = logica.OR(True, False)
+resultado_not = logica.NOT(True)
+print(resultado_and)  
+print(resultado_or)   
+print(resultado_not)  
+
+
